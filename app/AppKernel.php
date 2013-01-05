81b16a5adcd89a26575e966039ef116711e7f604
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-01-05 12:59:58 +0100

    [STARTUP] StofDoctrineExtensionsBundle installation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
