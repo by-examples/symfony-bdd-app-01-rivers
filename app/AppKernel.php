723250996f5aa878f50b5e0cd407221cb6aa08af
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-01-14 19:22:13 +0100

    [STARTUP] FOSUserBundle installation
    Conflicts:
    	app/config/config.yml
    	app/config/routing.yml
    	app/config/security.yml
    	composer.json
    
    Conflicts:
    	app/AppKernel.php
    	app/config/routing.yml
    
    Conflicts:
    	app/config/config.yml
    	app/config/security.yml
    	composer.json
    
    Conflicts:
    	app/AppKernel.php
    	app/config/config.yml
    	composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
