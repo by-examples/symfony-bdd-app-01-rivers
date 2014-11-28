bdc651ebd537c4d1d66c5415d3bd6c230bde2363
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-11-28 16:46:34 +0100

    [STARTUP] JmsSerializerBundle installation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
