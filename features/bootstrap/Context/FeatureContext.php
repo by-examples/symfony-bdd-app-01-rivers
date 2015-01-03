d6ea80cebaeea6c920bcb5de67b713c3e43ef28c
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-03 15:16:03 +0100

    PHP CS FIXER - fixed CS

--- features/bootstrap/Context/FeatureContext.php
+++ features/bootstrap/Context/FeatureContext.php
@@ -4,8 +4,6 @@ namespace Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 
 /**
