48c58cb5a8ec0330a8a6490a6afdcaad09f78db5
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-03 10:51:25 +0100

    [GREEN] Tweaks: menu option and reload.bash

--- reload.bash
+++ reload.bash
@@ -16,3 +16,6 @@ php app/console cache:warmup --env=prod
 
 mysql -u root < 00-extra/db/create-empty-database.sql
 php app/console doctrine:schema:update --force
+
+php app/console doctrine:fixtures:load -n
+php app/console fos:user:create admin admin@example.net loremipsum --super-admin
